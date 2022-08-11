55180d65fbb9947148f16a7679dd0ba13dca664c
darylbatchelor <daryl@batchelors.id.au>
2022-08-11 16:28:01 +1000

    GS-553: fixed view permissions in cm_info
    (cherry picked from commit 7893d0377bf5024a936b6f2a22bb574ac2f0ede0)

--- lib.php
+++ lib.php
@@ -29,6 +29,7 @@
  */
 
 use tool_organisation\api;
+use mod_facetoface\custom_capability_checker;
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -2784,6 +2785,7 @@ function facetoface_format_session_times($start, $end, $tz) {
 function facetoface_cm_info_view(cm_info $coursemodule) {
     global $USER, $DB;
     $output = '';
+    $capability_checker = new custom_capability_checker();
 
     if (!($facetoface = $DB->get_record('facetoface', ['id' => $coursemodule->instance]))) {
         return null;
@@ -2796,13 +2798,13 @@ function facetoface_cm_info_view(cm_info $coursemodule) {
         return null; // Not allowed to view this activity.
     }
     // Can view attendees.
-    $viewattendees = has_capability('mod/facetoface:viewattendees', $contextmodule);
+    $viewattendees = (has_capability('mod/facetoface:viewattendees', $contextmodule) || $capability_checker->manager_permissions);
     // Can see "view all sessions" link even if activity is hidden/currently unavailable.
-    $iseditor = has_any_capability([
+    $iseditor = (has_any_capability([
         'mod/facetoface:viewattendees', 'mod/facetoface:editsessions',
         'mod/facetoface:addattendees', 'mod/facetoface:addattendees',
         'mod/facetoface:takeattendance',
-    ], $contextmodule);
+    ], $contextmodule) || $capability_checker->manager_permissions);
 
     $timenow = time();
 
