72f4e15d7c8a1b19a51938ad753e5b7e00d5f211
Nicholas Lambell <nicholas.lambell@health.qld.gov.au>
2022-08-22 13:15:44 +1000

    Update facetoface_mycandidate_selector to use hierarchy my user queries instead of profile fields

--- lib.php
+++ lib.php
@@ -4340,14 +4340,28 @@ class facetoface_myexisting_selector extends user_selector_base {
     public function find_users($search) {
         global $USER, $DB;
 
-        $posid = $DB->get_record('user_info_field', array('shortname' => 'posid'));
-        $repdel = $DB->get_record('user_info_field', array('shortname' => 'repdel'));
-        $reportsto = $DB->get_record('user_info_field', array('shortname' => 'reportsto'));
-        $termination = $DB->get_record('user_info_field', array('shortname' => 'termination'));
-        $leave = $DB->get_record('user_info_field', array('shortname' => 'leave'));
+        $joins = $wheres = [];
 
-        // By default wherecondition retrieves all users except the deleted, not confirmed and guest.
-        list($wherecondition, $whereparams) = $this->search_sql($search, 'u');
+        list($wheres[], $params) = $this->search_sql($search, 'u');
+
+        [
+            'joins' => $myusersjoins,
+            'where' => $myuserswhere,
+            'params' => $myusersparams
+        ] = api::get_myusers_sql($USER->id, true);
+        if (!empty($myusersjoins)) {
+            $joins[] = $myusersjoins;
+        }
+        if (!empty($myuserswhere)) {
+            $wheres[] = $myuserswhere;
+        }
+        if (!empty($myusersparams)) {
+            $params = array_merge($params, $myusersparams);
+        }
+
+        // Prepare final values.
+        $joinsstring = implode("\n", $joins);
+        $where = implode(' AND ', $wheres);
 
         $fields  = 'SELECT ' . $this->required_fields_sql('u');
         $fields .= ', su.id AS submissionid, s.discountcost, su.discountcode, su.notificationtype, f.id AS facetofaceid,
@@ -4386,31 +4400,26 @@ class facetoface_myexisting_selector extends user_selector_base {
             JOIN
                 {user} u
              ON u.id = su.userid
-			 LEFT JOIN {user_info_data} ab
-					ON ab.userid = $userid AND ab.fieldid = $posid->id
-
-				  LEFT JOIN {user_info_data} ae
-					ON ae.userid = $userid AND ae.fieldid = $repdel->id
-
-				  INNER JOIN {user_info_data} aa
-					ON u.id = aa.userid AND aa.fieldid = $reportsto->id AND (aa.data=ab.data OR aa.userid = $userid OR
-					CASE WHEN ae.data > 1 THEN aa.data = ae.data END)
-
-				  LEFT JOIN {user_info_data} ac
-					ON u.id = ac.userid AND ac.fieldid = $termination->id
-				  LEFT JOIN {user_info_data} ad
-					ON u.id = ad.userid AND ad.fieldid = $leave->id
+             $joinsstring
 	            WHERE
-                $wherecondition
+                $where
             AND s.id = :sessid2
             AND ss.superceded != 1
             AND ss.statuscode >= :statusapproved
         ";
         $order = " ORDER BY sign.timecreated ASC, ss.timecreated ASC";
-        $params = array ('sessid1' => $this->sessionid, 'statusbooked' => MDL_F2F_STATUS_BOOKED, 'statuswaitlisted' => MDL_F2F_STATUS_WAITLISTED);
-        $params = array_merge($params, $whereparams);
-        $params['sessid2'] = $this->sessionid;
-        $params['statusapproved'] = MDL_F2F_STATUS_APPROVED;
+
+        $params = array_merge(
+            $params,
+            [
+                'sessid1' => $this->sessionid,
+                'sessid2' => $this->sessionid,
+                'statusapproved' => MDL_F2F_STATUS_APPROVED,
+                'statuswaitlisted' => MDL_F2F_STATUS_WAITLISTED,
+                'statusbooked' => MDL_F2F_STATUS_BOOKED,
+            ]
+        );
+
         if (!$this->is_validating()) {
             $potentialmemberscount = $DB->count_records_sql($countfields . $sql, $params);
             if ($potentialmemberscount > 100) {
