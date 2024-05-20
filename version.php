087eae703f7d5b9b477029114c8ee808191a4016
Kevin Pham <keevan.pham@gmail.com>
2024-05-20 10:31:17 +1000

    chore: bump version

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023101902;
-$plugin->release   = 2022101902;
+$plugin->version   = 2023101903;
+$plugin->release   = 2022101903;
 $plugin->requires  = 2022031500;  // Requires 4.0.
 $plugin->component = 'mod_facetoface';
 $plugin->maturity  = MATURITY_STABLE;
