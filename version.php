a041e71f5ecd68750e90c64a7e49b858f5f312b9
Matthew Hilton <matthewhilton@catalyst-au.net>
2024-07-09 11:24:15 +1000

    chore: bump version

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024070500;
-$plugin->release   = 2024070500;
+$plugin->version   = 2024070900;
+$plugin->release   = 2024070900;
 $plugin->requires  = 2023100900;  // Requires 4.3.
 $plugin->component = 'mod_facetoface';
 $plugin->maturity  = MATURITY_STABLE;
