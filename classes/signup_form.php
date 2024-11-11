61e309b9e4160663477fb006119da52b17aeb3b9
Nicholas Lambell <nicholas.lambell@health.qld.gov.au>
2024-11-12 09:53:18 +1000

    GS-553: Update element to properly use hidden field params

--- classes/signup_form.php
+++ classes/signup_form.php
@@ -64,16 +64,8 @@ class mod_facetoface_signup_form extends moodleform {
         }
         $mform->setType('discountcode', PARAM_TEXT);
 
-        $options = [
-            MDL_F2F_BOTH => get_string('notificationboth', 'facetoface'),
-            MDL_F2F_TEXT => get_string('notificationemail', 'facetoface'),
-            MDL_F2F_ICAL => get_string('notificationical', 'facetoface'),
-        ];
         // GCHLOL Change to hidden
-        $mform->addElement('hidden', 'notificationtype', get_string('notificationtype', 'facetoface'), $options);
-        $mform->addHelpButton('notificationtype', 'notificationtype', 'facetoface');
-        $mform->addRule('notificationtype', null, 'required', null, 'client');
-        $mform->setDefault('notificationtype', 1); // GCHLOL change default to 1
+        $mform->addElement('hidden', 'notificationtype', MDL_F2F_ICAL);
         $mform->setType('notificationtype', PARAM_INT); // GCHLOL: Set notificationtype type.
 
         $this->add_action_buttons(true, get_string('signup', 'facetoface'));
