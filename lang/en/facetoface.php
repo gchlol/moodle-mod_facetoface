bc90b784f3f0dce13af47dd484f36cd4a47ce231
Sajonasj-OCWSL <Jonas.Sajonas@health.qld.gov.au>
2025-02-19 11:10:11 +1000

    GS-712: changed language string of email to payroll number

--- lang/en/facetoface.php
+++ lang/en/facetoface.php
@@ -235,7 +235,7 @@ $string['facetoface:confirmandprocess'] = 'Confirm and process';
 $string['facetoface:uploadbookingsfile'] = 'Bookings file';
 $string['facetoface:uploadbookingsfiledesc'] = "
 Fields expected:
-- Email address (required)
+- Payroll Number (required)
 - Session number (required)
 - Discount code (optional)
 - Notification type (optional - valid options are 'email', 'ical', or 'both')
