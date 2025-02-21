7d2906ee4bb7e670c8e19ccd3f158bd257506ced
Sajonasj-OCWSL <Jonas.Sajonas@health.qld.gov.au>
2025-02-21 13:28:45 +1000

    GS-640: added draft bulk session upload file

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023101907;
-$plugin->release   = 2023101907;
+$plugin->version   = 2025021902;
+$plugin->release   = 2025021902;
 $plugin->requires  = 2022031500;  // Requires 4.0.
 $plugin->component = 'mod_facetoface';
 $plugin->maturity  = MATURITY_STABLE;
