4e387528f7f205980ba8a836d2342e081ca55b22
Sajonasj-OCWSL <Jonas.Sajonas@health.qld.gov.au>
2025-02-24 17:01:01 +1000

    GS-640: updated version.php to a higher version

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025021902;
-$plugin->release   = 2025021902;
+$plugin->version   = 2025021903;
+$plugin->release   = 2025021903;
 $plugin->requires  = 2022031500;  // Requires 4.0.
 $plugin->component = 'mod_facetoface';
 $plugin->maturity  = MATURITY_STABLE;
