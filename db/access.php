d008fa62abc01529a90f16c45859f8b2dd8fddf7
Sajonasj-OCWSL <Jonas.Sajonas@health.qld.gov.au>
2025-02-24 16:36:26 +1000

    GS-640: added access capability code

--- db/access.php
+++ db/access.php
@@ -217,4 +217,15 @@ $capabilities = [
             'manager' => CAP_ALLOW
         )
     ),
+
+    // GCHLOL MF Ability to upload and process bookings via CSV.
+    'mod/facetoface:uploadbulksessions' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => array(
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ),
+    ),
 ];
