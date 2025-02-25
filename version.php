9daa94044d8b2da897a3dff7c794857208403cda
Sajonasj-OCWSL <Jonas.Sajonas@health.qld.gov.au>
2025-02-25 11:54:46 +1000

    GS-640: version bump

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025021903;
-$plugin->release   = 2025021903;
+$plugin->version   = 2025021920;
+$plugin->release   = 2025021920;
 $plugin->requires  = 2022031500;  // Requires 4.0.
 $plugin->component = 'mod_facetoface';
 $plugin->maturity  = MATURITY_STABLE;
