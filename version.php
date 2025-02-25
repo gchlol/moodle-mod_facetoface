87da0f550d338931a8f83e7e401ec61bd25521de
Sajonasj-OCWSL <Jonas.Sajonas@health.qld.gov.au>
2025-02-25 11:22:00 +1000

    GS-712: edited example.csv to match the format required for the upload, version bump.

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025021901; 
-$plugin->release   = 2025021901;
+$plugin->version   = 2025021905; 
+$plugin->release   = 2025021905;
 $plugin->requires  = 2022031500;  // Requires 4.0.
 $plugin->component = 'mod_facetoface';
 $plugin->maturity  = MATURITY_STABLE;
