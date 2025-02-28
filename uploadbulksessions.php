a1a11f33102f1d1043a2356791a4c56c9cddd3fc
Sajonasj-OCWSL <Jonas.Sajonas@health.qld.gov.au>
2025-02-28 20:09:34 +1000

    GS-640: commented out page url at uploadbulksessions

--- uploadbulksessions.php
+++ uploadbulksessions.php
@@ -164,7 +164,7 @@ if ($process && $fileid && $f) {
 $mform = new bulk_session_upload_form(null, ['f' => $f]);
 $heading = get_string('facetoface:uploadbulksessions', 'facetoface');
 
-$PAGE->set_url(new moodle_url('/mod/facetoface/uploadbulksessions.php', ['f' => $f]));
+// $PAGE->set_url(new moodle_url('/mod/facetoface/uploadbulksessions.php', ['f' => $f]));
 $mform->set_data(['f' => $f, 'validate' => 1]);
 $PAGE->set_pagelayout('standard');
 $PAGE->set_title($heading);
