966eb24ad4fb9e284d1ec6167d237a8318fc9eea
Yucheng Zhu <Yucheng.Zhu@health.qld.gov.au>
2025-03-01 14:32:39 +1000

    GS-66: Made a dummy file to isolate the Attendance_Sheet_Table to simpliy development
    Delete me afterwards

---
+++ a.php
@@ -0,0 +1,17 @@
+<?php
+// a.php - Minimal solution to display the table.
+require_once('../../config.php');
+require_login();
+
+$PAGE->set_url(new moodle_url('/mod/facetoface/a.php'));
+$PAGE->set_context(context_system::instance());
+$PAGE->set_title('Attendance Sheet Settings');
+$PAGE->set_heading('Attendance Sheet Settings');
+
+require_once('attendance_sheet_settings.class.php');
+
+$attendance = new attendance_sheet_settings();
+
+echo $OUTPUT->header();
+echo $attendance->render();
+echo $OUTPUT->footer();
