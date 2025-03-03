7f6f2df5d8612ede12b1665bbc2d1256d05be603
Sajonasj-OCWSL <Jonas.Sajonas@health.qld.gov.au>
2025-03-03 16:46:45 +1000

    GS-641: added sitebulkupload.php

--- lang/en/facetoface.php
+++ lang/en/facetoface.php
@@ -838,17 +838,17 @@ $string['addtoallsessions'] = 'Add users to all (upcoming) sessions';
 $string['addtoallsessions_help'] = 'Use this option if you want to add users to all upcoming Face-to-Face sessions. When this uption is toggled, the selected users will be added to this session and all other future sessions in the activity.';
 $string['caseinsensitive'] = 'Case insensitive';
 
-// GCHLOL - MF - Removed delete from 'editsessions', added deletesessions, custom features
+// GCHLOL - MF - Removed delete from 'editsessions', added deletesessions, custom features.
 $string['facetoface:addmyattendees'] = 'Add my attendees to a Face-to-face session';
 $string['facetoface:deletesessions'] = 'Delete Face-to-face sessions';
 $string['facetoface:editsessions'] = 'Add, edit and copy Face-to-face sessions';
 $string['facetoface:removemyattendees'] = 'Remove my attendees from a Face-to-face session';
 
-// Core
+// Core.
 $string['session:strftimedate'] = '';
 $string['session:strftimetime'] = '';
 
-// Pages
+// Pages.
 $string['attendees:print'] = 'Print';
 $string['attendancesheet:heading'] = 'Attendance Sheet';
 $string['attendancesheet:logfacilitator'] = 'Facilitator';
@@ -857,7 +857,7 @@ $string['attendancesheet:signature'] = 'Signature';
 $string['modform:showlogo'] = 'Show site logo';
 $string['modform:showlogo_help'] = 'Controls whether the site logo is included in the header of the attendance sheet';
 
-// Enum
+// Enum.
 $string['attendancecolumn:0'] = 'Name';
 $string['attendancecolumn:1'] = 'Payroll';
 $string['attendancecolumn:2'] = 'Email';
@@ -865,3 +865,6 @@ $string['attendancecolumn:3'] = 'Org Unit';
 $string['attendancecolumn:4'] = 'Position';
 $string['attendancecolumn:5'] = 'Stream';
 $string['attendancecolumn:6'] = 'Paypoint';
+
+// Site wide bulk upload.
+$string['f2fbulksessions'] = 'Face-to-Face Bulk Upload sessions';
--- settings.php
+++ settings.php
@@ -195,3 +195,13 @@ $html .= html_writer::link($url, get_string('addnewnoticelink', 'facetoface'));
 $html .= html_writer::end_tag('p');
 
 $settings->add(new admin_setting_heading('facetoface/sitenotices_header', get_string('sitenoticesheading', 'facetoface'), $html));
+
+
+if ($hassiteconfig) { // Site admins only.
+    $ADMIN->add('modsettings', new admin_externalpage(
+        'facetoface_sitebulkupload',
+        get_string('f2fbulksessions', 'mod_facetoface'),
+        new moodle_url('/mod/facetoface/sitebulkupload.php'),
+        'moodle/site:config'
+    ));
+}
