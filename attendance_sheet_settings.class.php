c9d45032c76476bc8d906aa476922435e941afe6
Yucheng Zhu <Yucheng.Zhu@health.qld.gov.au>
2025-03-21 14:58:21 +1000

    GS-66: Copied Moodle style to the left config column.

--- attendance_sheet_settings.class.php
+++ attendance_sheet_settings.class.php
@@ -76,8 +76,8 @@ class attendance_sheet_settings {
         // Render the table using the Mustache template.
         $tablehtml = $OUTPUT->render_from_template('mod_facetoface/attendance_sheet_config_table', $this->data);
         // Wrap the table with a container that includes a left-hand label "Column config".
-        $output .= '<div class="attendance-sheet-config__wrapper" style="display: flex; align-items: flex-start;">';
-        $output .= '<div class="edw-form-label d-inline">Column config</div>';
+        $output .= '<div class="attendance-sheet-config__wrapper">';
+        $output .= '<div class="d-flex align-items-center flex-gap-1 inner edw-form-label d-inline">Column config</div>';
         $output .= '<div class="attendance-sheet-config__table">' . $tablehtml . '</div>';
         $output .= '</div>';
 
