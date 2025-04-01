6d27f79371be049030ba42b58eebfbab3fcddb2b
Jonas Sajonas <Jonas.Sajonas@health.qld.gov.au>
2025-04-01 14:55:11 +1000

    GS-641: refactor: confirm and upload forms

--- classes/form/site_bulk_session_confirm_form.php
+++ classes/form/site_bulk_session_confirm_form.php
@@ -14,27 +14,28 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * @package   mod_facetoface
- * @copyright 2025, Gold Coast Health
- * @author    Jonas Sajonas
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 namespace mod_facetoface\form;
+
 use moodleform;
 
 defined('MOODLE_INTERNAL') || die();
 require_once($CFG->libdir.'/formslib.php');
 
 /**
- * A Moodle form that collects confirmation details (e.g., suppressing emails)
- * before finalizing bulk session creation from CSV data.
+ * Confirmation form for bulk session uploads in Face-to-Face module.
+ * Allows users to proceed with session processing.
+ *
+ * @package   mod_facetoface
+ * @copyright 2025, Gold Coast Health
+ * @author    Jonas Sajonas
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class site_bulk_session_confirm_form extends moodleform {
 
     /**
-     * Defines elements and layouts on site-level confirmation form.
+     * Builds confirmation form for bulk session processing site level.
+     *
+     * @return void
      */
     public function definition() {
         $mform = $this->_form;
@@ -43,26 +44,25 @@ class site_bulk_session_confirm_form extends moodleform {
         $fileid = $this->_customdata['fileid'] ?? 0;
 
         // Checkbox to allow user to suppress email notifications.
-        $mform->addElement('advcheckbox', 'suppressemail', get_string('suppressemail', 'facetoface'));
+        $mform->addElement(
+            'advcheckbox',
+            'suppressemail',
+            get_string('suppressemail', 'facetoface')
+        );
+
         $mform->setType('suppressemail', PARAM_BOOL);
 
         // Hidden element to store file ID and pass it to form submission.
         $mform->addElement('hidden', 'fileid', $fileid);
         $mform->setType('fileid', PARAM_INT);
 
-        // Hidden marker to indicate we're processing this form.
         $mform->addElement('hidden', 'process', 1);
         $mform->setType('process', PARAM_INT);
 
-        // Button group: confirm or cancel.
-        $buttonarray = [];
-        $buttonarray[] = $mform->createElement(
-            'submit',
-            'confirmbtn',
+        // Add button group (confirm + cancel).
+        $this->add_action_buttons(
+            true,
             get_string('facetoface:confirmandprocess', 'mod_facetoface')
         );
-        $buttonarray[] = $mform->createElement('cancel');
-
-        $mform->addGroup($buttonarray, 'buttonar', '', ' ', false);
     }
 }
--- classes/form/site_bulk_session_upload_form.php
+++ classes/form/site_bulk_session_upload_form.php
@@ -35,7 +35,6 @@ require_once($CFG->libdir.'/formslib.php');
  * @author    Jonas Sajonas
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 class site_bulk_session_upload_form extends moodleform {
 
     /**
