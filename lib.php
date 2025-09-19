3973ab3aceefb7c1691db1b781c9998337a0163e
Matthew Fein <matthew.fein@health.qld.gov.au>
2025-09-19 15:04:32 +1000

    GS-961: Waitlist now orders by signup date

--- lib.php
+++ lib.php
@@ -589,7 +589,7 @@ function facetoface_update_attendees($session) {
     $course = $DB->get_record('course', ['id' => $facetoface->course]);
 
     // Update user status'.
-    $users = facetoface_get_attendees($session->id);
+    $users = facetoface_get_attendees_by_signup($session->id); // GCHLOL: Get attendees in signup order.
 
     if ($users) {
         // No/deleted session dates.
@@ -4765,4 +4765,76 @@ class facetoface_existing_selector extends user_selector_base {
         $options['file'] = 'mod/facetoface/lib.php';
         return $options;
     }
+}
+
+/**
+ * Get list of users attending a given session ordered by signup time.
+ *
+ * @param int $sessionid Session ID.
+ * @return stdClass[] List of user records with signup details.
+ */
+function facetoface_get_attendees_by_signup(int $sessionid): array {
+    global $DB;
+
+    $usernamefields = facetoface_get_all_user_name_fields(true, 'user');
+
+    $sql = "
+        SELECT  user.id,
+                $usernamefields,
+                user.email,
+                signups.id AS submissionid,
+                sessions.discountcost,
+                signups.discountcode,
+                signups.notificationtype,
+                facetoface.id AS facetofaceid,
+                facetoface.course,
+                signups_status.grade,
+                signups_status.statuscode,
+                latest_status.timecreated
+
+        FROM    {facetoface} facetoface
+                JOIN {facetoface_sessions} sessions ON
+                    sessions.facetoface = facetoface.id
+                JOIN {facetoface_signups} signups ON
+                    signups.sessionid = sessions.id
+                JOIN {facetoface_signups_status} signups_status ON
+                    signups_status.signupid = signups.id
+
+                LEFT JOIN (
+                    SELECT  signup_status.signupid,
+                            MAX(signup_status.timecreated) AS timecreated
+
+                    FROM    {facetoface_signups_status} signup_status
+                            JOIN {facetoface_signups} signup ON
+                                signup.id = signup_status.signupid AND
+                                signup.sessionid = :sessionid_sub
+
+                    WHERE   signup_status.statuscode IN (:status_booked, :status_waitlisted)
+
+                    GROUP BY
+                            signup_status.signupid
+                ) latest_status ON
+                    latest_status.signupid = signups.id
+
+                    JOIN {user} user ON
+                    user.id = signups.userid
+
+        WHERE   sessions.id = :sessionid_main AND
+                signups_status.superceded <> 1 AND
+                signups_status.statuscode >= :status_approved
+
+        ORDER BY
+                latest_status.timecreated ASC,
+                signups_status.timecreated ASC
+    ";
+
+    $params = [
+        'sessionid_sub' => $sessionid,
+        'status_booked' => MDL_F2F_STATUS_BOOKED,
+        'status_waitlisted' => MDL_F2F_STATUS_WAITLISTED,
+        'sessionid_main' => $sessionid,
+        'status_approved' => MDL_F2F_STATUS_APPROVED,
+    ];
+
+    return $DB->get_records_sql($sql, $params);
 }
