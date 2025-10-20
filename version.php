17bbdf5e3312e6b6d678318e900ebb33e46f6bae
Yucheng Zhu <Yucheng.Zhu@health.qld.gov.au>
2025-10-20 15:08:12 +1000

    GS-994: Fractional version increment.

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025081802;
+$plugin->version   = 2025081802.2;
 $plugin->release   = 2025081802;
 $plugin->requires  = 2023100900;  // Requires 4.3.
 $plugin->component = 'mod_facetoface';
