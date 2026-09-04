f896e2143e838fd04bbce5b31fa2a4ddd83ddcb4
Yucheng Zhu <Yucheng.Zhu@health.qld.gov.au>
2026-09-04 13:38:38 +1000

    GS-1258: Added Behat test for the course-level upload.
    Fixed a null instance variable bug when calling `new confirm_bookings_form(null)`

--- classes/form/confirm_bookings_form.php
+++ classes/form/confirm_bookings_form.php
@@ -62,7 +62,9 @@ class confirm_bookings_form extends moodleform {
         $mform->setType('caseinsensitive', PARAM_BOOL);
 
         $backurl = new moodle_url('/mod/facetoface/upload.php', ['f' => $f]);
-        $confirmbuttonlabel = $this->_customdata['haserrors'] ? 'updatevalidrows' : 'facetoface:confirmandprocess';
+        $confirmbuttonlabel = ($this->_customdata['haserrors'] ?? false)
+            ? 'updatevalidrows'
+            : 'facetoface:confirmandprocess';
         $htmlbuttons = $OUTPUT->render((new single_button(
             new moodle_url('/mod/facetoface/upload.php', ['f' => $f, 'fileid' => $fileid, 'process' => 1]),
             get_string($confirmbuttonlabel, 'mod_facetoface'),
